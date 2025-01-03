655d3d8414fbd2ceaa491f85ec0315ff6386f0d4
Miguel Figueiredo <miguelpfigueiredo@gmail.com>
2025-01-03 17:59:36 +0000

    Initial basic neovim configuration

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    extensions = { "fugitive", "lazy", "trouble", "neo-tree", "mason", "quickfix" },
+    options = {
+      theme = "dracula",
+      icons_enabled = true,
+      component_separators = { left = "󰿟", right = "󰿟" },
+      section_separators = { left = "", right = "" },
+    },
+  },
+}
+
