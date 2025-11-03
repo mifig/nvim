d1df05eb6bcbb2053d0b6f1e072ad05ac24ee4e1
Miguel Figueiredo <miguelpfigueiredo@gmail.com>
2025-11-03 12:42:26 +0000

    enabled visual surround plugin

--- lua/plugins/visual-surround.lua
+++ lua/plugins/visual-surround.lua
@@ -1,7 +1,6 @@
 -- Add surround chars in visual mode by just selecting and then typing the char
 return {
   "NStefan002/visual-surround.nvim",
-  enabled = false,
   opts = {
     use_default_keymaps = true,
     surround_chars = { "{", "}", "[", "]", "(", ")", "'", '"', "`" },
