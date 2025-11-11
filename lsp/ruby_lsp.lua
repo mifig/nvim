82329b05d668695c2e4141a44a2df76194e0df12
Miguel Figueiredo <miguelpfigueiredo@gmail.com>
2025-11-11 12:13:55 +0000

    Corrected ruby lsp not doing gd or Shift+k

--- lsp/ruby_lsp.lua
+++ lsp/ruby_lsp.lua
@@ -3,13 +3,7 @@ return {
   cmd = { vim.fn.expand("~/.rbenv/shims/ruby-lsp") },
   filetypes = { "ruby", "eruby" },
   root_markers = {
-    ".git",
     "Gemfile",
-    "Rakefile",
-    "config.ru",
-    "app.rb",
-    "config.ru",
-    "Gemfile.lock",
   },
   single_file_support = true,
 }
--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -1,10 +1,10 @@
 -- -- Enable lsp completion (what does this do? maybe i don't need it because of cmp?)
-vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("UserLspAttach", { clear = true }),
-  callback = function(ev)
-    vim.lsp.completion.enable(true, ev.data.client_id, ev.buf)
-  end,
-})
+-- vim.api.nvim_create_autocmd("LspAttach", {
+--   group = vim.api.nvim_create_augroup("UserLspAttach", { clear = true }),
+--   callback = function(ev)
+--     vim.lsp.completion.enable(true, ev.data.client_id, ev.buf)
+--   end,
+-- })
 
 -- Enable configured language servers
 -- server configurations are in lsp/*.lua and included automatically
